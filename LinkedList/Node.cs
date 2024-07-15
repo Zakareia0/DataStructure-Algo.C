ef5a575c4cda06e952bd4bd8aa500e5d7bdc4361
Zakareia Tarek Mohamed Aboulezz <211120018@aue.ae>
2024-07-15 11:51:40 +0400

    my message

---
+++ LinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Data_Structures_Course_2024.LinkedList
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node? Next { get; set; }
+
+        public void DisplayNode()
+        {
+            Console.WriteLine(Data);
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Data_Structures_Course_2024.Stack
+{
+    public class Stack
+    {
+        public int MaxSize { get; set; }
+        public string[] StackArray { get; set; }
+        public int Top { get; set; }
+
+        public Stack(int size)
+        {
+            this.MaxSize = size;
+            // creates Arrua with szie
+            this.StackArray = new string[MaxSize];
+            this.Top = -1;
+
+        }
+
+        public void Push(string item)
+        {
+            Top++;
+            StackArray[Top] = item;
+
+        }
+        public string Pop()
+        {
+            int old_top = Top;
+            Top--;
+            return StackArray[old_top];
+
+        }
+
+        public string Peek()
+        {
+            return StackArray[Top];
+        }
+
+        public bool isEmpty()
+        {
+            return Top == 0;
+        }
+
+        public bool isFull()
+        {
+            return  (MaxSize -1 == Top);
+        }
+    }
+}
